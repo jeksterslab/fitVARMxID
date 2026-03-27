4717ff5c18e9c64d14f36c826bb87d7a9857e291
jeksterslab <learn.jeksterslab@gmail.com>
2026-03-27 18:51:29 +0000

    Automated build.

--- tests/testthat/test-fitVARMxID-theta-fixed-false-sym-d-equal-true-null.R
+++ tests/testthat/test-fitVARMxID-theta-fixed-false-sym-d-equal-true-null.R
@@ -172,7 +172,7 @@ lapply(
         )
         testthat::expect_true(
           all(
-            obj$lbound == -650
+            obj$lbound == -30
           )
         )
         testthat::expect_true(
